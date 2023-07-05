e96c5144b8144675f444cc15485d9a8793ca51a9
宋鑫 <xin.song4@lotuscars.com.cn>
2023-07-05 15:30:08 +0800

    first commit

---
+++ Debug/0_Src/0_AppSw/Tricore/App/subdir.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C_FILES += "..\0_Src\0_AppSw\Tricore\App\GPIO.c"
+OBJ_FILES += "0_Src\0_AppSw\Tricore\App\GPIO.o"
+"0_Src\0_AppSw\Tricore\App\GPIO.o" : "..\0_Src\0_AppSw\Tricore\App\GPIO.c" "0_Src\0_AppSw\Tricore\App\.GPIO.o.opt"
+	@echo Compiling ${<F}
+	@"${PRODDIR}\bin\cctc" -f "0_Src\0_AppSw\Tricore\App\.GPIO.o.opt"
+
+"0_Src\0_AppSw\Tricore\App\.GPIO.o.opt" : .refresh
+	@argfile "0_Src\0_AppSw\Tricore\App\.GPIO.o.opt" -o "0_Src\0_AppSw\Tricore\App\GPIO.o" "..\0_Src\0_AppSw\Tricore\App\GPIO.c" -Ctc27xc --lsl-core=vtc -t -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN" -Wa-H"sfr/regtc27xc.def" -Wa-gAHLs --emit-locals=-equs,-symbols -Wa-Ogs -Wa--error-limit=42 -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\0_AppSw\Config\Common" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\2_CDrv\Tricore" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\0_AppSw" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\0_AppSw\Tricore" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\0_AppSw\Tricore\Main" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\If" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\StdIf" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\SysSe" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\Tricore" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\Tricore\Compilers" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore\_Impl" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore\_Lib" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore\_PinMap" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore\_Reg" --iso=99 --language=-gcc,-volatile,+strings,-kanji --fp-model=clfznrST --switch=auto --align=0 --default-near-size=8 --default-a0-size=0 --default-a1-size=0 -O2 --tradeoff=4 --compact-max-size=200 -g --error-limit=42 --source -c --dep-file="0_Src\0_AppSw\Tricore\App\.GPIO.o.d" -Wc--make-target="0_Src\0_AppSw\Tricore\App\GPIO.o"
+DEPENDENCY_FILES += "0_Src\0_AppSw\Tricore\App\.GPIO.o.d"
+
+C_FILES += "..\0_Src\0_AppSw\Tricore\App\MulitiCAN.c"
+OBJ_FILES += "0_Src\0_AppSw\Tricore\App\MulitiCAN.o"
+"0_Src\0_AppSw\Tricore\App\MulitiCAN.o" : "..\0_Src\0_AppSw\Tricore\App\MulitiCAN.c" "0_Src\0_AppSw\Tricore\App\.MulitiCAN.o.opt"
+	@echo Compiling ${<F}
+	@"${PRODDIR}\bin\cctc" -f "0_Src\0_AppSw\Tricore\App\.MulitiCAN.o.opt"
+
+"0_Src\0_AppSw\Tricore\App\.MulitiCAN.o.opt" : .refresh
+	@argfile "0_Src\0_AppSw\Tricore\App\.MulitiCAN.o.opt" -o "0_Src\0_AppSw\Tricore\App\MulitiCAN.o" "..\0_Src\0_AppSw\Tricore\App\MulitiCAN.c" -Ctc27xc --lsl-core=vtc -t -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN" -Wa-H"sfr/regtc27xc.def" -Wa-gAHLs --emit-locals=-equs,-symbols -Wa-Ogs -Wa--error-limit=42 -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\0_AppSw\Config\Common" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\2_CDrv\Tricore" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\0_AppSw" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\0_AppSw\Tricore" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\0_AppSw\Tricore\Main" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\If" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\StdIf" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\SysSe" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\Tricore" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\1_SrvSw\Tricore\Compilers" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore\_Impl" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore\_Lib" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore\_PinMap" -I"D:\WorkSpace\Tasking\Tasking_YuanLi_TC27xD_MulitiCAN\0_Src\4_McHal\Tricore\_Reg" --iso=99 --language=-gcc,-volatile,+strings,-kanji --fp-model=clfznrST --switch=auto --align=0 --default-near-size=8 --default-a0-size=0 --default-a1-size=0 -O2 --tradeoff=4 --compact-max-size=200 -g --error-limit=42 --source -c --dep-file="0_Src\0_AppSw\Tricore\App\.MulitiCAN.o.d" -Wc--make-target="0_Src\0_AppSw\Tricore\App\MulitiCAN.o"
+DEPENDENCY_FILES += "0_Src\0_AppSw\Tricore\App\.MulitiCAN.o.d"
+
+
+GENERATED_FILES += "0_Src\0_AppSw\Tricore\App\GPIO.o" "0_Src\0_AppSw\Tricore\App\.GPIO.o.opt" "0_Src\0_AppSw\Tricore\App\.GPIO.o.d" "0_Src\0_AppSw\Tricore\App\GPIO.src" "0_Src\0_AppSw\Tricore\App\GPIO.lst" "0_Src\0_AppSw\Tricore\App\MulitiCAN.o" "0_Src\0_AppSw\Tricore\App\.MulitiCAN.o.opt" "0_Src\0_AppSw\Tricore\App\.MulitiCAN.o.d" "0_Src\0_AppSw\Tricore\App\MulitiCAN.src" "0_Src\0_AppSw\Tricore\App\MulitiCAN.lst"
